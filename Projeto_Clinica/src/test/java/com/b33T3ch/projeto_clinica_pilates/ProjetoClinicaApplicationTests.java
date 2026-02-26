7631912306069a9dd253c30c4c19efdf4e9ec617
Gerson Cruz <gersonneto1908@gmail.com>
2026-02-26 09:15:27 -0400

    Projeto adicionado ao repositório GIT

---
+++ Projeto_Clinica/src/test/java/com/b33T3ch/projeto_clinica_pilates/ProjetoClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b33T3ch.projeto_clinica_pilates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
